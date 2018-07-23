35eb579ad4ea26e400e9535f8a68f6c9c644a95a
Gurinder Chauhan <gurinderchauhan@gmail.com>
2018-07-23 11:00:50 -0400

    wip

--- src/components/GoogleRecaptcha.vue
+++ src/components/GoogleRecaptcha.vue
@@ -2,7 +2,7 @@
     <div id="google-recaptcha-container" v-if="show">
         <h3 class="label">Verify you are human, Check the box below</h3>
         <div ref="captchaZone" style="min-height: 78px;"></div>
-        <input type="hidden" :name="name" v-model="response" v-if="input">
+        <input type="hidden" :name="input" v-model="response" v-if="input">
     </div>
 </template>
 
@@ -11,6 +11,10 @@
         name: "recaptcha",
 
         props: {
+            captchaScript: {
+                type: String,
+                default: 'https://www.google.com/recaptcha/api.js?render=explicit'
+            },
             dataShow: {
                 type: Boolean,
                 default: false
@@ -20,16 +24,10 @@
                 default: false
             },
             input: {
-                type: Boolean,
-                default: false
-            },
-            name: {
-                type: String,
-                default: 'gRecaptchaResponse'
+                type: String
             },
             siteKey: {
-                type: String,
-                required: true
+                type: String
             },
             theme: {
                 type: String,
@@ -39,34 +37,60 @@
 
         data() {
             return {
-                show: true,
+                show: false,
                 captchaId: null,
                 response: null,
                 captcahLoaded: false,
                 options: {
-                    'callback': this.verifyCallback,
-                    'sitekey': this.siteKey,
-                    'theme': this.theme
+                    callback: this.verifyCallback,
+                    sitekey: this.siteKey ? this.siteKey : window.App.recaptcha.sitekey,
+                    theme: this.theme
                 }
             }
         },
 
         mounted() {
-            if (!this.captcahLoaded) {
-                this.checkCaptchaLoaded();
-            }
+            this.loadScript();
         },
 
         methods: {
 
+            loadScript() {
+                let head = document.getElementsByTagName("head")[0];
+
+                let script = document.createElement("script");
+                script.charset = "utf-8";
+                script.type = "text/javascript";
+                script.async = true;
+                script.id = this.captchaScript;
+                script.src = this.captchaScript;
+                script.loadJS = "watermark";
+                head.appendChild(script);
+
+                this.checkCaptchaLoaded();
+            },
+
             checkCaptchaLoaded(timeId = null) {
                 if (window.hasOwnProperty('grecaptcha') && window.grecaptcha.hasOwnProperty('render')) {
+
                     this.captcahLoaded = true;
+
                     clearTimeout(timeId);
-                    this.captchaId = window.grecaptcha.render(this.$refs.captchaZone, this.options)
+
+                    if(this.dataShow === true) {
+                        this.show = true;
+                    } else this.show = this.dataShow !== false;
+
+
                 } else {
                     let timeId = setTimeout(() => this.checkCaptchaLoaded(timeId), 500);
                 }
+            },
+
+            render() {
+                this.$nextTick(() => {
+                    this.captchaId = window.grecaptcha.render(this.$refs.captchaZone, this.options);
+                })
             },
 
             verifyCallback(response) {
@@ -76,7 +100,7 @@
                     response: response,
                     captchaId: this.captchaId
                 })
-            },
+            }
         },
 
         watch: {
@@ -88,9 +112,7 @@
             },
 
             dataShow: function (val) {
-                if (!val) {
-                    this.show = false
-                }
+                this.show = !!val;
             },
 
             dataReset: function (newVal, oldVal) {
